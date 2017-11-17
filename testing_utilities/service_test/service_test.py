9cebe1ada4f8769ff8c3bfd85c3797eebb7d7108
arne-he <arne.hennies@hotmail.de>
2017-11-17 17:11:05 +0100

    add: config, dictionarys, jobspooler

---
+++ testing_utilities/service_test/service_test.py
@@ -0,0 +1,13 @@
+from docker import types
+import docker
+
+client = docker.from_env()
+low_client = docker.APIClient()
+
+
+service = low_client.services(filters={'id': 'ncjg26qpluge'})[0]
+task_template = service['Spec']['TaskTemplate']
+curr_replicas = service['Spec']['Mode']['Replicated']['Replicas']
+curr_labels = service["Spec"]["Labels"]
+mode = types.ServiceMode(mode='replicated', replicas=curr_replicas)
+low_client.update_service(service["ID"], version=service["Version"]["Index"], task_template=task_template,name=service["Spec"]["Name"], labels=curr_labels, mode=mode)
